f93a148e9a16567d088fafabeb56821656a52cfb
Lucas Teske <lucas@teske.com.br>
2023-01-23 03:08:08 -0300

    Removed firmware (now in another repository)

--- pico-input/firmware/include/ctrl.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void handleUART();
-void healthCheckAction();
-void setError();
--- pico-input/firmware/include/pinctrl.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-void initGPIO();
-uint16_t readGPIO();
-void reportGPIO(uint8_t myId, uint16_t gpio);
-uint8_t getAddr();
